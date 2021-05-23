4768c4f0a5f2a9ccc986343f2dc32b520453d1c8
Matt Schaefer <matt@situatedbit.com>
2021-05-23 08:02:56 -0700

    initial commit

---
+++ bin/fetch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+STATION=$1
+OUT_DIR=$2
+OUT=$OUT_DIR/$(date +%Y-%m-%d-%s)-$STATION.html
+curl "https://forecast.weather.gov/product.php?site=$STATION&issuedby=$STATION&product=AFD&format=txt&version=1&glossary=0" > $OUT
